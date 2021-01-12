102a95252080a7a5f0c45c085d684de4484060aa
brandaoplaster <brandaoplaster@gmail.com>
2021-01-11 23:50:18 -0300

    chore: installing dependencies

--- mix.exs
+++ mix.exs
@@ -42,7 +42,13 @@ defmodule GoCarApi.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:comeonin, "~> 4.0"},
+      {:argon2_elixir, "~> 1.2"},
+      {:absinthe, "~> 1.4"},
+      {:absinthe_plug, "~> 1.4"},
+      {:absinthe_ecto, "~> 0.1.3"},
+      {:guardian, "~> 2.0"}
     ]
   end
 
