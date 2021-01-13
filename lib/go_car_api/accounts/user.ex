9bffdc41355abe3d5f4483bdbb3395b77b0a0176
brandaoplaster <brandaoplaster@gmail.com>
2021-01-13 19:52:22 -0300

    chore: adding context user and creating migrate

---
+++ lib/go_car_api/accounts/user.ex
@@ -0,0 +1,23 @@
+defmodule GoCarApi.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "users" do
+    field :email, :string, unique: true
+    field :password_hash, :string
+    field :password, :string, virtual: true
+    field :password_confirmation, :string, virtual: true
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:email])
+    |> validate_required([:email])
+    |> unique_constraint(:email)
+  end
+end
