ea4bef887fd9da083a24d04a6bed422e7788dce8
brandaoplaster <brandaoplaster@gmail.com>
2021-01-14 22:28:51 -0300

    feat: adding validation for user

--- lib/go_car_api/accounts/user.ex
+++ lib/go_car_api/accounts/user.ex
@@ -16,8 +16,21 @@ defmodule GoCarApi.Accounts.User do
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:email])
-    |> validate_required([:email])
+    |> cast(attrs, [:email, :password, :password_confirmation])
+    |> validate_required([:email, :password, :password_confirmation])
+    |> validate_format(:email, ~r/@/)
+    |> update_change(:email, &String.downcase(&1))
+    |> validate_length(:password, min: 6, max: 100)
+    |> validate_confirmation(:password)
     |> unique_constraint(:email)
+    |> hash_password
+  end
+
+  defp hash_password(%{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, Comeonin.Argon2.add_hash(password))
+  end
+
+  defp hash_password(changeset) do
+    changeset
   end
 end
